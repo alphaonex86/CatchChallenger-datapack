35b81db869072376f85c12f6ede7f71b3a7800fe
BRULE <tgjklmda@hotmail.Fr>
2014-08-30 12:23:11 +0200

    skin plant map quests

--- datapack/quests/1102/client_logic.js
+++ datapack/quests/1102/client_logic.js
@@ -7,3 +7,14 @@ if(currentQuestStep()==1)
 	else
 		return 42;//need more object
 }
+if(currentQuestStep()==2)
+{
+	if(haveQuestStepRequirements())
+		return 12;//give all object to pass to step 2
+	else
+		return 52;//need more object
+}
+
+if(currentQuestStep()==3)
+
+	return 15;//to finish the quest
