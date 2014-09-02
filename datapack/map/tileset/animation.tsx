53e31f97da56e3c2434f2f507d46ab857ff4020d
BRULE <tgjklmda@hotmail.Fr>
2014-09-02 12:06:35 +0200

    refonte skin player

--- datapack/map/tileset/animation.tsx
+++ datapack/map/tileset/animation.tsx
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="animation" tilewidth="16" tileheight="16">
+ <properties>
+  <property name="animation" value="500ms;4frames;random-offset"/>
+ </properties>
  <image source="animation.png" trans="000000" width="512" height="512"/>
  <tile id="0">
   <properties>
