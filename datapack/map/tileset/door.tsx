870837e46858a27252a7daba1634101a9e35ec31
BRULE Herman <alpha_one_x86@first-world.info>
2017-08-03 02:30:12 +0200

    Fix the doors tilset

--- datapack/map/tileset/door.tsx
+++ datapack/map/tileset/door.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="door" tilewidth="16" tileheight="32">
+<tileset name="door" tilewidth="16" tileheight="32" tilecount="512" columns="32">
  <image source="door.png" trans="000000" width="512" height="512"/>
+ <tile id="4">
+  <properties>
+   <property name="animation" value="100ms;6frames"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="animation" value="100ms;6frames"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="animation" value="100ms;6frames"/>
+  </properties>
+ </tile>
+ <tile id="132">
+  <properties>
+   <property name="animation" value="100ms;6frames"/>
+  </properties>
+ </tile>
+ <tile id="164">
+  <properties>
+   <property name="animation" value="100ms;6frames"/>
+  </properties>
+ </tile>
+ <tile id="196">
+  <properties>
+   <property name="animation" value="100ms;6frames"/>
+  </properties>
+ </tile>
 </tileset>
