3e77148fa3cafd40ebca1f2ac4eb50a734aab9eb
BRULE Herman <alpha_one_x86@first-world.info>
2018-05-14 18:54:33 +0200

    Clean

--- datapack/map/main/test/quests/1001/client_logic.js
+++
@@ -1,9 +0,0 @@
-if(currentQuestStep()==0)
-	return 1;//do you want accept the quest
-if(currentQuestStep()==1)
-{
-	if(haveQuestStepRequirements())
-		return 2;//give all object to pass to step 2
-	else
-		return 42;//need more object
-}
