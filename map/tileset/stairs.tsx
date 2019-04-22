7d6bf1b5f4079a77f5c4e096a2c84ce2719a457d
BRULE Herman <alpha_one_x86@first-world.info>
2019-04-22 09:18:01 -0400

    [Add] stairs for map generation

---
+++ map/tileset/stairs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="stairs" tilewidth="16" tileheight="16" tilecount="10" columns="5">
+ <image source="stairs.png" width="80" height="32"/>
+</tileset>
