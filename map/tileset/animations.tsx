9046b44623c09ddf2c29b1a25611565e4a11ab0a
BRULE Herman <alpha_one_x86@first-world.info>
2019-05-19 10:41:56 -0400

    [Improvement] Start basic test with new tileset

--- map/tileset/animations.tsx
+++ map/tileset/animations.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="animations.tsx" tilewidth="16" tileheight="16">
+<tileset name="animations.tsx" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
  <image source="animations.png" width="512" height="512"/>
  <tile id="0">
   <properties>
@@ -16,7 +16,7 @@
    <property name="animation" value="100ms;6frames"/>
   </properties>
  </tile>
-  <tile id="96">
+ <tile id="96">
   <properties>
    <property name="animation" value="100ms;6frames"/>
   </properties>
@@ -26,84 +26,36 @@
    <property name="animation" value="100ms;6frames"/>
   </properties>
  </tile>
- <tile id="320">
-  <properties>
-   <property name="trigger" value="start:62ms;4frames;leave:62ms;4frames;again:62ms;3frames;over"/>
-  </properties>
- </tile>
- <tile id="352">
-  <properties>
-   <property name="trigger" value="start:62ms;4frames;leave:62ms;3frames;over"/>
-  </properties>
- </tile>
  <tile id="256">
   <properties>
    <property name="animation" value="100ms;6frames"/>
   </properties>
  </tile>
-  <tile id="288">
+ <tile id="288">
   <properties>
    <property name="animation" value="100ms;6frames"/>
   </properties>
  </tile>
-  <tile id="320">
+ <tile id="320">
+  <properties>
+   <property name="animation" value="100ms;6frames"/>
+   <property name="trigger" value="start:62ms;4frames;leave:62ms;4frames;again:62ms;3frames;over"/>
+  </properties>
+ </tile>
+ <tile id="352">
+  <properties>
+   <property name="animation" value="100ms;6frames"/>
+   <property name="trigger" value="start:62ms;4frames;leave:62ms;3frames;over"/>
+  </properties>
+ </tile>
+ <tile id="384">
   <properties>
    <property name="animation" value="100ms;6frames"/>
   </properties>
  </tile>
-  <tile id="352">
+ <tile id="416">
   <properties>
-   <property name="animation" value="100ms;6frames"/>
-  </properties>
- </tile>
-  <tile id="384">
-  <properties>
-   <property name="animation" value="100ms;6frames"/>
-  </properties>
- </tile>
-  <tile id="416">
-  <properties>
-   <property name="animation" value="100ms;6frames"/>
-  </properties>
- </tile>
-  <tile id="448">
-  <properties>
-   <property name="animation" value="100ms;6frames"/>
-  </properties>
- </tile>
-  <tile id="480">
-  <properties>
-   <property name="animation" value="100ms;6frames"/>
-  </properties>
- </tile>
-  <tile id="512">
-  <properties>
-   <property name="animation" value="100ms;6frames"/>
-  </properties>
- </tile>
-  <tile id="544">
-  <properties>
-   <property name="animation" value="100ms;6frames"/>
-  </properties>
- </tile>
-  <tile id="576">
-  <properties>
-   <property name="animation" value="100ms;6frames"/>
-  </properties>
- </tile>
-  <tile id="608">
-  <properties>
-   <property name="animation" value="100ms;6frames"/>
-  </properties>
- </tile>
-  <tile id="640">
-  <properties>
-   <property name="animation" value="100ms;6frames"/>
-  </properties>
- </tile>
-  <tile id="672">
-  <properties>
-   <property name="animation" value="100ms;6frames"/>
+   <property name="animation" value="400ms;9frames;random-offset"/>
   </properties>
  </tile>
 </tileset>
